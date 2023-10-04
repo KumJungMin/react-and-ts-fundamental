1e78eb6329edd9a4d6d0bd2d02de196a996f56ff
KumJungMin <rmawjdals@naver.com>
2023-10-04 20:39:14 +0900

    2-3. 숫자, 불리언 타입 선언 방법

--- variables.ts
+++ variables.ts
@@ -5,3 +5,15 @@ movieTitle = 'Interstellar2';
 movieTitle = 123; // error
 movieTitle.toUpperCase(); // ok
 movieTitle.toUpper(); // error
+
+
+// 2. declare a number type variable
+let numCatLives: number = 9;
+numCatLives += 1; // ok
+numCatLives = '9'; // error
+
+
+// 3. declare a boolean type variable
+let isDone: boolean = false;
+isDone = true; // ok
+isDone = 1; // error
