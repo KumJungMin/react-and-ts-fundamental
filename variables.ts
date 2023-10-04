81eceac111795d9e02be4e69fac665f96dfdbd49
KumJungMin <rmawjdals@naver.com>
2023-10-04 20:37:53 +0900

    1. string 타입 선언 방법

---
+++ variables.ts
@@ -0,0 +1,7 @@
+// 1. declare a string type variable
+let movieTitle: string = 'Interstellar';
+movieTitle = 'Interstellar2';
+
+movieTitle = 123; // error
+movieTitle.toUpperCase(); // ok
+movieTitle.toUpper(); // error
