7789f5547c4ec561fbfa9081b6d297273a997e08
KumJungMin <rmawjdals@naver.com>
2023-10-10 19:15:46 +0900

    16. 객체 혹은 함수 리턴 객체에 타입 지정하는 법

--- objects.ts
+++ objects.ts
@@ -6,3 +6,14 @@ printName({first: 2}); // error
 printName(dog); // error
 printName({first: 'Elton'}); // error
 printName({first: 'Elton', last: 'John'}); // ok
+
+// 16-1. 객체를 정의할 때, 타입을 정의할 수 있다.({인자명:타입} 형식)
+let coordinate : {x: number, y: number,} = { x: 10, y: 20 }
+
+// 16-2. 함수에서 리턴하는 객체 타입을 정의할 수 있다.
+function randomCoordinate(): {x: number, y: number} {
+  return { 
+    x: Math.random(), 
+    y: Math.random() 
+  }
+}
