9bdcb41c69483e59757fb8260ccd0bedcae7d861
KumJungMin <rmawjdals@naver.com>
2023-10-12 18:37:43 +0900

    23. 배열에 커스텀 타입 지정 가능

--- arrays.ts
+++ arrays.ts
@@ -7,3 +7,15 @@ activeUsers.push('Max');    // error
 const activeUsers2: string[] = []; 
 activeUsers2.push('Max');    // ok
 activeUsers2.push(123);      // error
+
+// 23-1. Array<타입> 방식으로 타입을 지정할 수도 있다.
+const bools1: Array<boolean> = [];
+const bools2: boolean[] = [];
+
+// 23-2. 커스텀 타입을 배열의 타입으로 지정할 수도 있다.
+// type Point = {
+//   x: number,
+//   y: number,
+// }
+const coords: Point[] = [];
+coords.push({ x: 0, y: 0 }); // ok
