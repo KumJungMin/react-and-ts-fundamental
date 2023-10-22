44feb2a829981470c1a2d16163f1a0d1483759bf
KumJungMin <rmawjdals@naver.com>
2023-10-22 13:16:02 +0900

    31-2. 인터페이스는 ?로 선택적 프로퍼티를 정의한다

--- interfaces.ts
+++ interfaces.ts
@@ -6,3 +6,13 @@ interface Point {
 const pt: Point = { x: 0, y: 0 }; // ok
 const pt2: Point = { x: 0 }; // error
 const pt3: Point = {}; // error
+
+
+// 인터페이스는 ?로 선택적 프로퍼티를 정의한다.
+interface Person {
+  name: string;
+  age?: number;
+}
+const person: Person = { name: 'Jane' }; // ok
+const person2: Person = { name: 'Jane', age: 22 }; // ok
+const person3: Person = {}; // error
