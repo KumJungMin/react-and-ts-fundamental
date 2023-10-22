6a71f918eeb20b22f36395c56c9902294b271879
KumJungMin <rmawjdals@naver.com>
2023-10-22 13:34:25 +0900

    31-7. 쉼표(,)를 사용해 다중상속으로 interface 확장 가능

--- interfaceVsType.ts
+++ interfaceVsType.ts
@@ -14,3 +14,23 @@ const dog: Dog = { name: 'Jane', birth: 1992 }; // ok
 interface ServiceDog extends Dog {
   job: 'drug sniffing' | 'bomb sniffing' | 'rescue' | 'therapy';
 }
+
+
+// 3. 쉼표(,)를 사용해 다중상속으로 interface를 확장할 수 있음
+interface Person {
+  name: string;
+}
+interface Employee {
+  readonly id: number;
+  email: string; 
+}
+interface Engineer extends Person, Employee {
+  favoriteLanguage: string;
+}
+
+const tony: Engineer = {
+  name: 'Tony',
+  id: 1,
+  email: '',
+  favoriteLanguage: 'TypeScript',
+};
